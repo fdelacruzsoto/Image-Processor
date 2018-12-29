94bea784f0a4261f6c0dfa184551d038dc4cb132
Fernando de la Cruz <fdelacruzsoto@gmail.com>
2018-12-28 20:04:05 -0600

    IM-PR-01 Read files in a given directory specified by a constant

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-const cron = require('node-cron');
+const fs = require('fs');
+// const cron = require('node-cron');
+const { promisify } = require('util');
 
-cron.schedule('* * * * *', () => {
-  console.log('running a task every minute');
-});
+// We'll be using this constant to set where the job should look for images.
+const IMG_PATH = './images';
+
+// We apply promisify to readdir function so that we can use it with async await.
+const readdir = promisify(fs.readdir);
+
+const readImagesFromDir = async () => {
+  try {
+    const images = await readdir(IMG_PATH);
+    return images;
+  } catch (error) {
+    return [];
+  }
+};
+
+const startProcessing = async () => {
+  const images = await readImagesFromDir();
+  images.forEach((img) => {
+    console.log(img);
+  });
+};
+
+startProcessing();
+
+/* cron.schedule('* * * * *', () => {
+  processImages();
+}); */
