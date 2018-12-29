27987cc8fa58d6e3f6bbb6611c8b5535010c3c66
Fernando De La Cruz Soto <fdelacruzsoto@gmail.com>
2018-12-28 20:39:19 -0600

    Revert "IM-PR-01	Read files"

--- index.js
+++ index.js
@@ -1,39 +1,5 @@
-const fs = require('fs');
-// const cron = require('node-cron');
-const { promisify } = require('util');
+const cron = require('node-cron');
 
-// We'll be using this constant to set where the job should look for images.
-const FILES_PATH = './images';
-
-// We apply promisify to readdir function so that we can use it with async await.
-const readdir = promisify(fs.readdir);
-
-/**
- * Read the files inside the specified path directory, return an array with the files names,
- * if there is any error tryng to read the list of files then an empty array should be returned.
- * @returns {Array} files
- */
-const readFilesFromDir = async () => {
-  try {
-    const files = await readdir(FILES_PATH);
-    return files;
-  } catch (error) {
-    return [];
-  }
-};
-
-/**
- * Main function used to start processing the images.
- */
-const startProcessing = async () => {
-  const files = await readFilesFromDir();
-  files.forEach((img) => {
-    console.log(img);
-  });
-};
-
-startProcessing();
-
-/* cron.schedule('* * * * *', () => {
-  processImages();
-}); */
+cron.schedule('* * * * *', () => {
+  console.log('running a task every minute');
+});
