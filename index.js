fb11017749b3781e4e507258af4f8a30772b45d4
Fernando de la Cruz <fdelacruzsoto@gmail.com>
2019-01-02 18:41:24 -0600

    IM-PR-03 Add documentation

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const { promisify } = require('util');
 
 // We'll be using this constant to set where the job should look for images.
 const FILES_PATH = './images';
-// This constant is used to filter the file list and keep only the images.
+// This constant is used to filter the file list and keep only the images. For simplicity
+// only jpg images will be processed.
 const REGEX_IMAGE = /\.(jpg)$/i;
 
 // We apply promisify to readdir function so that we can use it with async await.
@@ -24,8 +25,13 @@ const readFilesFromDir = async () => {
   }
 };
 
+/**
+ * Loops trough the list of files and keeps only the images specified in the constant.
+ * @param files The list of files to be sanitized.
+ * @returns {Array} imgs The sanitized list of images.
+ */
 const sanitizeFileList = (files) => {
-  const imgs = files.filter(img => REGEX_IMAGE.test(img) );
+  const imgs = files.filter(img => REGEX_IMAGE.test(img));
   return imgs;
 };
 
